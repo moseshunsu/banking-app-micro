fc37c65830fc5c0f288b3e654ccfe74763b8f5d0
Moses Hunsu <hunsumoses7@gmail.com>
2023-07-25 11:16:04 +0100

    Added a gateway server or an Identity management file

---
+++ identity-management/src/main/java/bankingMicroservice/identitymanagement/IdentityManagementApplication.java
@@ -0,0 +1,13 @@
+package bankingMicroservice.identitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityManagementApplication.class, args);
+	}
+
+}
