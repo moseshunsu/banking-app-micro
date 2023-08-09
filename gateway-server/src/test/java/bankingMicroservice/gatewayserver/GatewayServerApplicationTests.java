bfed8370a446522b47b5e3482fcb683a396ef652
Moses Hunsu <hunsumoses7@gmail.com>
2023-08-09 10:51:11 +0100

    Initial commit for gateway server

---
+++ gateway-server/src/test/java/bankingMicroservice/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bankingMicroservice.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
